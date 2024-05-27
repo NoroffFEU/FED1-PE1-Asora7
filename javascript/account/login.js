e8ad359956f46d8cc0fe8f924807d0e1e8c268f3
Asora7 <verstr01474@stud.noroff.no>
2024-05-27 21:19:29 +0200

    k

--- javascript/account/login.js
+++ javascript/account/login.js
@@ -42,7 +42,7 @@ document.addEventListener('DOMContentLoaded', function() {
         event.preventDefault();
 
         localStorage.removeItem('token');
-        window.location.href = 'index.html';
+        window.location.href = '/index.html';
     });
 
     function handleLogin(email, password) {
