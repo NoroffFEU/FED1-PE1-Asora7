320028c2ef544d45fac46b67c8b7064749e3a83b
Asora7 <verstr01474@stud.noroff.no>
2024-05-27 22:39:51 +0200

    e

--- javascript/account/login.js
+++ javascript/account/login.js
@@ -74,7 +74,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
             if (data.data && data.data.accessToken) {
                 localStorage.setItem('token', data.data.accessToken);
-                window.location.href = 'post/edit.html'; 
+                window.location.href = '../../post/edit.html'; 
 
                 const successMessage = document.querySelector('.success-message');
                 successMessage.textContent = 'Login successful!';
