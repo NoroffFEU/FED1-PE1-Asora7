5ae4c03d2bc2a8bf795f96afbcad453f05a720bd
Asora7 <verstr01474@stud.noroff.no>
2024-05-27 23:18:01 +0200

    d

--- javascript/account/register.js
+++ javascript/account/register.js
@@ -5,39 +5,35 @@ document.addEventListener('DOMContentLoaded', function() {
     const errorMessage = document.querySelector('.error-message');
     const registerButton = document.querySelector('button');
 
-    // registerButton.addEventListener('click', function(event) {
-    //     event.preventDefault();
-        
-    //     errorMessage.textContent = '';
-
-    //     const name = nameInput.value.trim();
-    //     const email = emailInput.value.trim();
-    //     const password = passwordInput.value;
-
-    //     if (name === '' || email === '' || password === '') {
-    //         errorMessage.textContent = 'Please fill out all fields.';
-    //         return;
-    //     }
-
-    //     if (password.length < 8) {
-    //         errorMessage.textContent = 'Password must be at least 8 characters long.';
-    //         return;
-    //     }
-
-    //     const registrationData = {
-    //         name: name,
-    //         email: email,
-    //         password: password
-    //     };
-
-    //     fetchRegistration(registrationData);
-    // });
-
     registerButton.addEventListener('click', function(event) {
-        console.log('Registration successful:');
-        window.location.href = 'login.html'; 
+        event.preventDefault();
+        
+        errorMessage.textContent = '';
 
+        const name = nameInput.value.trim();
+        const email = emailInput.value.trim();
+        const password = passwordInput.value;
+
+        if (name === '' || email === '' || password === '') {
+            errorMessage.textContent = 'Please fill out all fields.';
+            return;
+        }
+
+        if (password.length < 8) {
+            errorMessage.textContent = 'Password must be at least 8 characters long.';
+            return;
+        }
+
+        const registrationData = {
+            name: name,
+            email: email,
+            password: password
+        };
+
+        fetchRegistration(registrationData);
     });
+
+
 
     function fetchRegistration(registrationData) {
         return fetch('https://v2.api.noroff.dev/auth/register', {
